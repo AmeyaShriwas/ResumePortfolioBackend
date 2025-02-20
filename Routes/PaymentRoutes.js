7171d6f33e6f41937681d7ed3e12a59b1f23e752
AmeyaShriwas133 <shriwas.ameya@webseeder.in>
2025-02-21 00:49:20 +0530

    payment

--- Routes/PaymentRoutes.js
+++ Routes/PaymentRoutes.js
@@ -5,5 +5,5 @@ const Router = express.Router();
 
 Router.post("/create-order", PaymentController.createOrderHandler);
 Router.post("/verify-payment", PaymentController.verifyPaymentHandler);
-
+//
 module.exports = Router;
