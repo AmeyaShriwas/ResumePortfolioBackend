910726886d290f077b7aa47475c9a54513487fbc
AmeyaShriwas133 <shriwas.ameya@webseeder.in>
2025-02-21 19:54:34 +0530

    payment

--- Routes/PortfolioRoutes.js
+++ Routes/PortfolioRoutes.js
@@ -3,17 +3,18 @@ const router = express.Router();
 const upload = require("./../Middleware/Multer");
 const PortfolioController = require("./../Controller/PortfolioController");
 
-// Add Portfolio
-router.post(
-  "/add",
-  upload.fields([
-    { name: "profilePhoto", maxCount: 1 },
-    { name: "resume", maxCount: 1 },
-    { name: "projects[].projectImage", maxCount: 10 }
-  ]),
-  PortfolioController.addPortfolio
-);
 
+
+router.post(
+    "/add",
+    upload.fields([
+      { name: "profilePhoto", maxCount: 1 },
+      { name: "resume", maxCount: 1 },
+      { name: "projectImages", maxCount: 10 } // Change from projects[].projectImage
+    ]),
+    PortfolioController.addPortfolio
+  );
+  
 // Get All Portfolios
 router.get("/all", PortfolioController.getAllPortfolios);
 
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const cors = require("cors");
 const mongoose = require('mongoose');
 require('dotenv').config();
 const authRoutes = require('./Routes/AuthRoutes');
